1d664a90788882626cc370024ecb6ac9b482cb57
sudiptapanda2004 <sanjupanda2003@gmail.com>
2026-03-22 17:19:34 +0530

    Merge remote-tracking branch 'origin/master'

--- src/CheckingPrime.java
+++ src/CheckingPrime.java
@@ -7,7 +7,7 @@ void main() {
     }
     else {
         boolean isPrime = true;
-        for (int i=2; i<=Math.sqrt(n); i++) {       // this is the correct way, to check very large numbers sometimes
+    for (int i=2; i<=Math.sqrt(n); i++) {  // root n method is the correct way, to check very large numbers sometimes
         // for (int i = 2; i <= n - 1; i++) {
             if (n % i == 0) {           // n is a multiple of i (n is not-equal to 1 or i)
                 isPrime = false;
